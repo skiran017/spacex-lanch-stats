a295b27a3b592421f42cac45598b8f474f3fb31b
skiran017 <skiran017@gmail.com>
2022-03-19 07:06:46 +0530

    graphql server and schema

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+// const cors = require('cors');
+const schema = require('./schema');
+// const path = require('path');
+
+const app = express();
+
+// Allow cross-origin
+// app.use(cors());
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
